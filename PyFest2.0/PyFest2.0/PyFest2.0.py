0fd490fbc69bbbdb22e8bba39dea5b68484de340
Goofy <jpin347@learner.dvusd.org>
2026-01-12 13:57:18 -0700

    first part of Pyfest2.0
    this is the first version of pyfest2.0, i just have the beginning part done

---
+++ PyFest2.0/PyFest2.0/PyFest2.0.py
@@ -0,0 +1,22 @@
+print("--- Py-fest 2026 Stage Manager ---")
+print("1. View lineup & total time")
+print("2. Add a New Band")
+print("3. Move First Band to End (Late Arrival)")
+print("4. Remove a Band by Name")
+print("5. Move Band to Specific Position")
+print("6. Exit")
+choice = input("Select an option (1-6): ")
+
+
+
+
+#Parts
+if choice == "1":
+
+elif choice == "2":
+elif choice == "3":
+elif choice == "4":
+elif choice == "5":
+    name_to_move = input("Enter the name of the band to move: ")
+elif choice == "6":
+    break 
