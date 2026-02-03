236ea3b3c4e164c5adffb92a3fb936a282fb6461
Goofy <jpin347@learner.dvusd.org>
2026-02-03 13:51:57 -0700

    fixing Car program
    I fixed the asthetic a little, and also added input

--- Cortet/Cortet/Cortet.py
+++ Cortet/Cortet/Cortet.py
@@ -18,28 +18,18 @@ E1 = [
     "E1","Mitsubishi Carisma GT", 225,(313,290),6000,5.2,1996,4]
 A2 = [
     "A2","Ford Focus WRC",224,(221,300),5400,5.5,1995,4]
+cars = [D1,H4,D4,E4,H2,D3,F3,F2,E1,A2]
 def print_all(c):
-    print(f"|{c[0]}|")
-    print(f"Car Model: {c[1]}")
-    print(f"Max Speed: {c[2]} km/h")
-    print(f"RPM at Max Power: {c[4]} rpm")
-    print(f"0-100 km/h: {c[5]} seconds")
-    print(f"Engine Capacity: {c[6]} cc")
-    print(f"Number of Cylinders: {c[7]}")
-    print("---------------")
-#print_all(D1)
-#print_all(H4)
-#rint_all(D4)
-#print_all(E4)
-#print_all(H2)
-#print_all(D3)
-#print_all(F3)
-#print_all(F2)
-#print_all(E1)
-#print_all(A2)
-print("models")
-print("--------")
-print("A2,D1,D3,D4,E1,E4,F2,F3,H2,H4,")
-question = input("which model do you want to see?: ")
-if question == "D1":
-    print_all(D1)
+    print("-----------------------------------------------------------")
+    print(f"|{c[0]}                                                            |")
+    print(f"|Car Model: {c[1]}       Max Speed: {c[2]} km/h     |")
+    print(f"|RPM at Max Power: {c[4]} rpm  0-100 km/h: {c[5]} seconds |")
+    print(f"|Engine Capacity: {c[6]} cc    Number of Cylinders: {c[7]}|")
+    print("|-----------------------------------------------------------")
+i = 1
+for car in cars:
+    print(car[1])
+    i += 1
+print("-----------------------------------------------------------")
+choice = int(input("Choose a car number: "))
+print_all(cars[choice - 1])
